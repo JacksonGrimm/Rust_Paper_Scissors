81509bc906ff30c8d1cd4a17569184f3acd20715
Jackson Grimm <jacksonGrimm18@gmail.com>
2023-11-25 21:21:56 -0700

    adds intro logs

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,26 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    /*
-    create a new variable with the name random_number and an inter type of unsigned size
-    that is equal to the thread_rng().gen_range method from the rand cargo
-    */
+    println!("==============================================");
+    println!("Welcome to Rust Paper Scissors!");
+    println!("Simply type: r, p, s");
+    println!("or q to quit");
+    println!("==============================================");
+
     let moves: [&str; 3] = ["r", "p", "s"];
 
     loop {
         //creates a new mutable variable equal to a string
         let mut player_move: String = String::new();
+        /*
+        create a new variable with the name random_number and an inter type of unsigned size
+        that is equal to the thread_rng().gen_range method from the rand cargo
+        */
         let random_number: usize = rand::thread_rng().gen_range(1..3);
         let computer_move: &str = moves[random_number];
 
         println!("");
         println!("Type your move!");
-        println!("Computer chose: {}", computer_move);
 
         io::stdin()
             // here I reference the string by borrowing it and say I would like to mutate it
@@ -26,6 +31,8 @@ fn main() {
         //trim method return string slice
         let trim_player_move: String = player_move.trim().to_lowercase();
         let trim_player_move_ref: &str = trim_player_move.as_str();
+
+        println!("Computer chose: {}", computer_move);
 
         if trim_player_move_ref == computer_move {
             println!("Tie Game!");
